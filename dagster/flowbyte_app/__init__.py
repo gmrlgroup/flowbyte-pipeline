2afe96efc51f5734f358ef9b7e723a743745bec0
Romy Bou Abdo <romy.bouabdo@gmrlgroup.com>
2025-04-10 17:28:20 +0300

    integration: upate get_db_credentials extraction and adjust paths based on operating system and partitions

--- dagster/flowbyte_app/__init__.py
+++ dagster/flowbyte_app/__init__.py
@@ -32,7 +32,7 @@ governance_assets = load_assets_from_modules([governance])
 
 
 ## INTEGRATION JOBS ##
-db_2_db_job = define_asset_job(name="db_2_db", selection=["get_db_credentials", "get_table_mapping", "get_field_mapping", "get_source_data", "transform_data", "transform_attributes", "add_destination_data", "add_destination_attributes"])
+db_2_db_job = define_asset_job(name="db_2_db", selection=["get_table_mapping", "get_field_mapping", "get_source_data", "transform_data", "transform_attributes", "add_destination_data", "add_destination_attributes"])
 
 db_2_duckdb_job = define_asset_job(name="db_2_duckdb", selection=["get_table_mapping_duckdb", "get_field_mapping_duckdb", "get_source_data_duckdb", "transform_data_duckdb", "add_destination_data_duckdb"])
 
@@ -44,7 +44,7 @@ duckdb_2_duckdb_job = define_asset_job(name="duckdb_2_duckdb", selection=["get_t
                                                                           "transform_data_duckdb_duckdb", 
                                                                           "add_destination_data_duckdb_duckdb"])
 
-duckdb_2_db_job = define_asset_job(name="duckdb_2_db", selection=["get_db_credentials_1",
+duckdb_2_db_job = define_asset_job(name="duckdb_2_db", selection=[
                                                                             "get_table_mapping_duckdb_db", 
                                                                           "get_field_mapping_duckdb_db", 
                                                                           "get_source_data_duckdb_db", 
